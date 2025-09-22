431aeb9eb0d801529ee49c9e67782eef6c70051f
Victor Guerrero <victorg@hey.com>
2025-09-23 00:11:07 +0200

    wip

--- public/themes/xivarri/front-page.php
+++ public/themes/xivarri/front-page.php
@@ -1,12 +1,27 @@
 <?php get_header(); ?>
 
-<div class="px-12 py-20">
-    <div class="max-w-lg font-medium text-3xl/tighter">Un espai de reflexió i diàleg per als qui tenen ganes de fer soroll. Aquí no vens només a escoltar, hi vens a dir la teva.</div>
+<div class="absolute inset-x-0 top-0 h-auto">
+    <img src="<?= asset('texture.svg') ?>" alt="" class="w-full h-auto" />
+</div>
 
-    <div class="font-medium uppercase mt-10">Despertem la ment</div>
-    <div class="font-black text-7xl uppercase">Xivarri</div>
+<div class="relative px-5 sm:px-12 py-20">
+    <div class="ml-auto max-w-lg font-medium text-3xl/tighter">Un espai de reflexió i diàleg per als qui tenen ganes de fer soroll. Aquí no vens només a escoltar, hi vens a dir la teva.</div>
 
-    <div class="text-sm text-orange uppercase mt-10">El silenci és necessari.<br>Però el soroll, deixa empremta.</div>
+    <div class="mx-auto mt-40">
+        <div class="font-medium text-4xl uppercase mt-10">Despertem la ment</div>
+        <div class="font-display font-black text-[84px] sm:text-[120px] md:text-[180px] lg:text-[240px] xl:text-[320px] 2xl:text-[350px] uppercase whitespace-nowrap leading-[0.75] *:transition-all *:duration-500 group overflow-y-hidden">
+            <span class="text-[1.25em] group-hover:text-[1em]">X</span><!--
+        --><span class="text-[0.95em]">i</span><!--
+        --><span class="text-[1.15em] -mr-[0.1em]">v</span><!--
+        --><span class="text-[1em] group-hover:text-[1.526em]">a</span><!--
+        --><span class="text-[1.05em] group-hover:text-[1.15em]">r</span><!--
+        --><span class="text-[1.4em] group-hover:text-[1em]">r</span><!--
+        --><span class="text-[1.075em]">i</span><!--
+        --><span class="text-[1.526em]">&#8203;</span>
+        </div>
+    </div>
+
+    <div class="text-sm text-orange uppercase mt-96">El silenci és necessari.<br>Però el soroll, deixa empremta.</div>
     <div class="max-w-3xl font-medium text-3xl/tighter mt-10">Fer Xivarri és una manera d’expressar inquietuds, de compartir idees, de contrastar opinions. És el que et permet despertar aquella curiositat que, en realitat, mai ha estat adormida. És l’idioma dels que pensen.
     </div>
 
@@ -52,5 +67,10 @@
 
         ?>
     </div>
+
+    <?php component('dyptich') ?>
+
+    <div class="h-screen"></div>
+    <div class="h-screen"></div>
 
 <?php get_footer(); ?>
--- public/themes/xivarri/header.php
+++ public/themes/xivarri/header.php
@@ -5,5 +5,5 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
 </head>
-<body class="bg-gradient-to-b from-blue to-blue-dark text-white antialiased">
+<body class="bg-gradient-to-b from-blue to-blue-dark text-white antialiased tracking-wide">
   <?php component('site-header') ?>
