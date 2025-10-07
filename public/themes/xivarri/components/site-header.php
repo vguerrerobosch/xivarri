fe59aa5b4b3d32ca196a26db88a7cd30e89ecce7
Victor Guerrero <victorg@hey.com>
2025-10-07 09:49:31 +0200

    wip

--- public/themes/xivarri/components/site-header.php
+++ public/themes/xivarri/components/site-header.php
@@ -1,4 +1,4 @@
-<div class="flex px-5 py-6 justify-between items-center space-x-10 uppercase leading-none -mb-24">
+<div class="relative flex px-5 py-6 justify-between items-center space-x-10 uppercase leading-none -mb-24 z-10">
     <div>Barcelona • Espanya<br>13 de novembre DE 2025<br>AULA MAGNA, CAMPUS UIC BARCELONA
     </div>
     <ul class="font-semibold flex space-x-10 ml-auto">
@@ -8,5 +8,5 @@
         <li><a href="#" class="hover:underline underline-offset-4 decoration-2">On som</a></li>
         <li><a href="#" class="bg-white text-blue-darker rounded-full py-2 px-6 hover:bg-apricot transition-colors hover:text-white">Compra l’entrada</a></li>
     </ul>
-    <div>Cat</div>
+    <div class="font-semibold uppercase">Cat</div>
 </div>
