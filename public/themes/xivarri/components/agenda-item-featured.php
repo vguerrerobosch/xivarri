3eeeb2cad73983350dbbf222cb34eea915394f59
Victor Guerrero <victorg@hey.com>
2025-10-07 18:47:59 +0200

    wip

--- public/themes/xivarri/components/agenda-item-featured.php
+++ public/themes/xivarri/components/agenda-item-featured.php
@@ -9,7 +9,7 @@ $args = wp_parse_args($args, [
 
 ?>
 <div class="border border-white/20 bg-white text-black rounded rounded-xl px-4 lg:px-6 py-4">
-    <div class="w-20 opacity-70"><?= $args['time'] ?></div>
+    <div class="w-20 opacity-70 text-lg"><?= $args['time'] ?></div>
     <div class="uppercase mt-12"><?= $args['title'] ?></div>
     <div class="font-semibold text-3xl"><?= $args['speaker'] ?></div>
     <div class="text-sm"><?= $args['description'] ?></div>
