9ca28aa0138c13c5fa85e8cd9dee14765ae9feab
Victor Guerrero <victorg@hey.com>
2025-10-07 13:31:50 +0200

    wip

--- public/themes/xivarri/components/site-header.php
+++ public/themes/xivarri/components/site-header.php
@@ -2,10 +2,19 @@
     <div>Barcelona • Espanya<br>13 de novembre DE 2025<br>AULA MAGNA, CAMPUS UIC BARCELONA
     </div>
     <ul class="font-semibold flex space-x-10 ml-auto">
-        <li><a href="#" class="hover:underline underline-offset-4 decoration-2">Què és?</a></li>
-        <li><a href="#" class="hover:underline underline-offset-4 decoration-2">Edicions passades</a></li>
-        <li><a href="#" class="hover:underline underline-offset-4 decoration-2">Agenda</a></li>
-        <li><a href="#" class="hover:underline underline-offset-4 decoration-2">On som</a></li>
+        <?php
+        $menu_items = [
+            ['url' => '#que-es', 'text' => 'Què és?'],
+            ['url' => '#edicions-passades', 'text' => 'Edicions passades'],
+            ['url' => '#agenda', 'text' => 'Agenda'],
+            ['url' => '#on-som', 'text' => 'On som']
+        ];
+        foreach ($menu_items as $item): ?>
+            <li><a
+                href="<?= $item['url']; ?>"
+                class="underline decoration-transparent hover:decoration-white underline-offset-4 decoration-2 transition-colors"
+            ><?= $item['text']; ?></a></li>
+        <?php endforeach; ?>
         <li><a href="#" class="bg-white text-blue-darker rounded-full py-2 px-6 hover:bg-apricot transition-colors hover:text-white">Compra l’entrada</a></li>
     </ul>
     <div class="font-semibold uppercase">Cat</div>
